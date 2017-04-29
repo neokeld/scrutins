db5285cd6f77177fd3d72e066b99fc77b6941e26
lucas <lucas.gautheron@gmail.com>
2017-04-29 14:26:23 +0200

    unidecode candidates

--- transform_2017.py
+++ transform_2017.py
@@ -1,6 +1,7 @@
 import csv
 import codecs
 from collections import deque
+from unidecode import unidecode
 
 lignes = []
 
@@ -14,7 +15,7 @@ with codecs.open('raw/pres_2017.csv', mode='r', encoding='iso-8859-1') as csvfil
 
         ligne_base = ['1'] + row[:21]
         for i in range(1, 12):
-            row[21+(i-1)*7+2] = row[21+(i-1)*7+2][:4]
+            row[21+(i-1)*7+2] = unidecode(row[21+(i-1)*7+2][:4])
             lignes.append(ligne_base + row[21+(i-1)*7 + 1:21+i*7+1])
 
 with open('data/pres_2017.csv', 'w') as csvfile:
